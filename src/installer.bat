a7f5e66ff7ef2cbf0883d85af7dc2d55b43a682f
ziemniakekk <124462298+deziemniakv@users.noreply.github.com>
2025-11-23 15:32:12 +0100

    Add files via upload

---
+++ src/installer.bat
@@ -0,0 +1,20 @@
+@echo off
+title FluxUtility - Installator
+
+echo Installing FluxUtility... [1/3] Installed Packages
+timeout 1 >nul
+cls
+
+echo Installing FluxUtility.. [2/3] Installed Packages
+timeout 1 >nul
+cls
+
+echo Installing FluxUtility. [3/3] Installed Packages 
+timeout 1 >nul
+cls
+
+echo The installation was successful. I'm launching the FluxUtility program.
+timeout 1 >nul
+cls
+
+python main.py
